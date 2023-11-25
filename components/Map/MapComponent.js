9bbdb61e1e162b310a2a7f2aa1df322b36c2bbd1
Brian <brian@brianaderer.com>
2023-11-25 11:57:51 -0500

    bootstrapped svg markers

--- components/Map/MapComponent.js
+++ components/Map/MapComponent.js
@@ -62,14 +62,20 @@ const MapComponent = ({ center, zoom, locationData }) => {
 
 
     function createElement({markerInstance, index ,AdvancedMarkerElement, PinElement, title, position}) {
-        const svg = <BiHealth/>;
-        console.log(svg);
-        const icon = document.createElement('div');
-
-        icon.innerHTML = svg;
-
-        const faPin = new PinElement({
-            glyph: icon,
+        const parser = new DOMParser();
+// A marker with a custom inline SVG.
+        const pinSvgString = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
+</svg>
+`;
+        const pinSvg = parser.parseFromString(
+            pinSvgString,
+            "image/svg+xml",
+        ).documentElement;
+        const glyphImg = document.createElement('img');
+        glyphImg.src = `https://developers.google.com/maps/documentation/javascript/examples/full/images/google_logo_g.svg`;
+        const glyphSvgPinElement = new PinElement({
+            glyph: pinSvg,
             glyphColor: "#ff8300",
             background: "#FFD514",
             borderColor: "#ff8300",
@@ -78,7 +84,7 @@ const MapComponent = ({ center, zoom, locationData }) => {
                         position: position,
                         map,
                         title: title,
-                        content: faPin.element,
+                        content: glyphSvgPinElement.element,
                         collisionBehavior: 'REQUIRED_AND_HIDES_OPTIONAL',
                         //content: markerInstance,
                     });
