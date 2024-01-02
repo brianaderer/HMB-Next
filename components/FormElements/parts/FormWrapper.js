d42cece44e1684486ce6d1bbc95f941f4d1b0da9
Brian <brian@brianaderer.com>
2024-01-01 19:55:07 -0500

    base styles laid down

--- components/FormElements/parts/FormWrapper.js
+++ components/FormElements/parts/FormWrapper.js
@@ -4,7 +4,7 @@ const FormWrapper = props => {
     const {children, handleSubmit, prompt, anchor} = props;
     return(
         <main {...(anchor?.length > 0 ? { id: anchor } : {})}  >
-            <form className="shadow-xl flex flex-col px-8 py-8 drop-shadow-lg bg-base-100/90" onSubmit={handleSubmit}>
+            <form className="flex flex-col px-8" onSubmit={handleSubmit}>
                 <h1 className="text-2xl font-bold">{prompt}</h1>
                 {children}
             </form>
