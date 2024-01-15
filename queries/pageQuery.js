d7c43d5d49a338ab4a352dfd23ec234e1b54f7e1
Brian <brian@brianaderer.com>
2024-01-14 23:24:43 -0500

    refactor the query

--- queries/pageQuery.js
+++ queries/pageQuery.js
@@ -1,13 +1,35 @@
 import {gql} from "@apollo/client";
 import {BlogInfoFragment} from "../fragments/GeneralSettings";
 import {FeaturedImage, NavigationMenu} from "../components";
+import components from '../wp-blocks';
+
 import {fragments, blocks} from '../zblocks';
 
 export const pageQuery = gql`
   ${BlogInfoFragment}
   ${NavigationMenu.fragments.entry}
   ${FeaturedImage.fragments.entry}
-  ${blocks}
+    ${components.CoreParagraph.fragments.entry}
+  ${components.CoreHeading.fragments.entry}
+  ${components.CoreButtons.fragments.entry}
+  ${components.CoreButton.fragments.entry}
+  ${components.CoreColumns.fragments.entry}
+  ${components.CoreColumn.fragments.entry}
+  ${components.CoreList.fragments.entry}
+  ${components.CoreMediaText.fragments.entry}
+  ${components.CoreSeparator.fragments.entry}
+  ${components.CoreImage.fragments.entry}
+  ${components.AcfGallery.fragments.entry}
+  ${components.AcfContactForm.fragments.entry}
+  ${components.AcfSignUpForm.fragments.entry}
+  ${components.AcfUserInfoForm.fragments.entry}
+  ${components.AcfHmbMap.fragments.entry}
+  ${components.AcfUploadMediaForm.fragments.entry}
+  ${components.CreateBlockAttractions.fragments.entry}
+  ${components.AcfGuestBook.fragments.entry}
+  ${components.CreateBlockReviews.fragments.entry}
+  ${components.CreateBlockGuestbook.fragments.entry}
+  ${components.CreateBlockInstagramGallery.fragments.entry}
   query GetPageData(
     $databaseId: ID!
     $headerLocation: MenuLocationEnum
@@ -22,7 +44,27 @@ export const pageQuery = gql`
         renderedHtml
         id: clientId
         parentClientId
-        ${fragments}
+        ...${components.CoreParagraph.fragments.key}
+        ...${components.CoreHeading.fragments.key}
+        ...${components.CoreButtons.fragments.key}
+        ...${components.CoreButton.fragments.key}
+        ...${components.CoreColumns.fragments.key}
+        ...${components.CoreColumn.fragments.key}
+        ...${components.CoreSeparator.fragments.key}
+        ...${components.CoreMediaText.fragments.key}
+        ...${components.CoreImage.fragments.key}
+        ...${components.CoreList.fragments.key}
+        ...${components.AcfGallery.fragments.key}
+        ...${components.AcfContactForm.fragments.key}
+        ...${components.AcfSignUpForm.fragments.key}
+        ...${components.AcfUserInfoForm.fragments.key}
+        ...${components.AcfHmbMap.fragments.key}
+        ...${components.AcfUploadMediaForm.fragments.key}
+        ...${components.CreateBlockAttractions.fragments.key}
+        ...${components.AcfGuestBook.fragments.key}
+        ...${components.CreateBlockReviews.fragments.key}
+        ...${components.CreateBlockGuestbook.fragments.key}
+        ...${components.CreateBlockInstagramGallery.fragments.key}
       }
       ...FeaturedImageFragment
     }
--- zblocks/blocks.js
+++ zblocks/blocks.js
@@ -2,26 +2,5 @@ import components from '../wp-blocks';
 import {gql} from '@apollo/client';
 
 const blocks = gql`
-  ${components.CoreParagraph.fragments.entry}
-  ${components.CoreHeading.fragments.entry}
-  ${components.CoreButtons.fragments.entry}
-  ${components.CoreButton.fragments.entry}
-  ${components.CoreColumns.fragments.entry}
-  ${components.CoreColumn.fragments.entry}
-  ${components.CoreList.fragments.entry}
-  ${components.CoreMediaText.fragments.entry}
-  ${components.CoreSeparator.fragments.entry}
-  ${components.CoreImage.fragments.entry}
-  ${components.AcfGallery.fragments.entry}
-  ${components.AcfContactForm.fragments.entry}
-  ${components.AcfSignUpForm.fragments.entry}
-  ${components.AcfUserInfoForm.fragments.entry}
-  ${components.AcfHmbMap.fragments.entry}
-  ${components.AcfUploadMediaForm.fragments.entry}
-  ${components.CreateBlockAttractions.fragments.entry}
-  ${components.AcfGuestBook.fragments.entry}
-  ${components.CreateBlockReviews.fragments.entry}
-  ${components.CreateBlockGuestbook.fragments.entry}
-  ${components.CreateBlockInstagramGallery.fragments.entry}
 `
 export default blocks;
