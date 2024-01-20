a7526e1dfa85b5ca5028a7e477099a6125e67f1f
Brian <brian@brianaderer.com>
2024-01-20 16:20:16 -0500

    sticky back working the old way

--- components/Map/MapComponent.js
+++ components/Map/MapComponent.js
@@ -289,7 +289,7 @@ const MapComponent = ({ center, zoom, locations, classes }) => {
         }
             </div>
         {categories.length > 0 &&
-            <Sticky target={'mapDiv'} classes={`w-1/4 p-2 ml-8 rounded bg-neutral drop-shadow-lg left-full`}>
+            <Sticky target={'mapDiv'} classes={`w-1/4 p-2 ml-8 absolute rounded bg-neutral drop-shadow-lg left-full`}>
             <form>
                 <p className={`p-2 text-center text-neutral-content`}>Click a category to show its contents.</p>
                 <fieldset className={`flex flex-col`}>
