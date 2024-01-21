7ecb15c17db649f131673d6a4c15e63b7a19f9ef
Brian <brian@brianaderer.com>
2024-01-21 11:56:06 -0500

    add null protection to media query

--- components/Gallery/parts/Masonry.js
+++ components/Gallery/parts/Masonry.js
@@ -7,7 +7,7 @@ import {ScreenContext} from "../../../contexts";
 
 const Masonry = props => {
     const screen = useContext(ScreenContext);
-    const isMobile = !screen.breakpoints.includes('md');
+    const isMobile = !screen?.breakpoints?.includes('md');
     const {posts, srcName, captionName, increment, cols, spacing} = props;
     const computedCols = isMobile ? 1 : cols;
     const computedIncrement = isMobile ? 7 : increment;
