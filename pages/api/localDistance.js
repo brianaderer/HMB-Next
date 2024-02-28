bc2533965988280dd5ad2e3b8e6ae9a700f92c10
Brian <brian@brianaderer.com>
2024-02-27 21:44:25 -0500

    deactivate distance api

--- pages/api/localDistance.js
+++ pages/api/localDistance.js
@@ -7,20 +7,21 @@ export default async function handler(req, res) {
 
     try {
         const client = new Client({});
-        const filePath = path.join(process.cwd(), 'data', 'locations.json');
-
-        // Read and parse the locations file
-        let locations = {};
-        try {
-            const fileContents = await fsPromises.readFile(filePath, 'utf8');
-            locations = JSON.parse(fileContents);
-            return res.status(200).json({ error: null, data: locations});
-        } catch (e) {
-            console.log('Failed to read or parse locations file, defaulting to empty object.', e);
-        }
-    } catch (error) {
-        // Catch-all error handling
-        console.error('Unexpected error:', error);
-        return res.status(500).json({ error: error.message });
-    }
+        return {};
+    //     const filePath = path.join(process.cwd(), 'data', 'locations.json');
+    //
+    //     // Read and parse the locations file
+    //     let locations = {};
+    //     try {
+    //         const fileContents = await fsPromises.readFile(filePath, 'utf8');
+    //         locations = JSON.parse(fileContents);
+    //         return res.status(200).json({ error: null, data: locations});
+    //     } catch (e) {
+    //         console.log('Failed to read or parse locations file, defaulting to empty object.', e);
+    //     }
+    // } catch (error) {
+    //     // Catch-all error handling
+    //     console.error('Unexpected error:', error);
+    //     return res.status(500).json({ error: error.message });
+    // }
 }
--- pages/api/writeDistance.js
+++ pages/api/writeDistance.js
@@ -4,18 +4,19 @@ import path from "path";
 
 export default async function handler(req, res) {
     const { distances } = req.body || {};
-
-    try {
-        const filePath = path.join(process.cwd(), 'data', 'locations.json');
-        try {
-            const success = await fsPromises.writeFile(filePath, JSON.stringify(distances, null, 2), 'utf8');
-            return res.status(200).json({ error: null, data: success});
-        } catch (e) {
-            console.log('Failed to read or parse locations file, defaulting to empty object.', e);
-        }
-    } catch (error) {
-        // Catch-all error handling
-        console.error('Unexpected error:', error);
-        return res.status(500).json({ error: error.message });
-    }
+    return {};
+    //
+    // try {
+    //     const filePath = path.join(process.cwd(), 'data', 'locations.json');
+    //     try {
+    //         const success = await fsPromises.writeFile(filePath, JSON.stringify(distances, null, 2), 'utf8');
+    //         return res.status(200).json({ error: null, data: success});
+    //     } catch (e) {
+    //         console.log('Failed to read or parse locations file, defaulting to empty object.', e);
+    //     }
+    // } catch (error) {
+    //     // Catch-all error handling
+    //     console.error('Unexpected error:', error);
+    //     return res.status(500).json({ error: error.message });
+    // }
 }
