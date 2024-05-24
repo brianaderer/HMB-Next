81d3006f802b533490da2abb7496f7df0c4f9a02
Brian <brian@brianaderer.com>
2024-05-24 14:20:50 -0400

    correctly install ga4

--- components/SEO/SEO.js
+++ components/SEO/SEO.js
@@ -21,15 +21,6 @@ export default function SEO({ title, description, imageUrl, url }) {
       <Head>
         <meta property="og:type" content="website" />
         <meta property="twitter:card" content="summary_large_image" />
-          <!-- Google tag (gtag.js) -->
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-BGVLN882LC"></script>
-          <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-BGVLN882LC');
-          </script>
 
         {title && (
           <>
@@ -62,6 +53,18 @@ export default function SEO({ title, description, imageUrl, url }) {
           </>
         )}
       </Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BGVLN882LC"></script>
+        <script
+            dangerouslySetInnerHTML={{
+                __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.GA4_TAG}');
+            `,
+            }}
+        />
     </>
   );
 }
