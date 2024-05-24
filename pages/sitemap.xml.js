9370504a2398dfae69bfb1ba1e151945273df61a
Brian <brian@brianaderer.com>
2024-05-24 13:54:03 -0400

    prune the sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -7,5 +7,15 @@ export default function Sitemap() {}
 export function getServerSideProps(ctx) {
     return getSitemapProps(ctx, {
         frontendUrl: process.env.NEXT_PUBLIC_WORDPRESS_URL,
+        sitemapPathsToIgnore: [
+            '/wp-sitemap-posts-post-*',
+            '/wp-sitemap-posts-attractions-*',
+            '/wp-sitemap-posts-review-*',
+            '/wp-sitemap-taxonomies-category-*',
+            '/wp-sitemap-taxonomies-place-category-*',
+            '/wp-sitemap-taxonomies-placetype-*',
+            '/wp-sitemap-users-*',
+            '/wp-sitemap-posts-guest-book-entry-*',
+        ]
     });
 }
