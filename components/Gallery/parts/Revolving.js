cf39388ad936d443fc70e5bb19519810f5003f24
Brian Aderer <brian@brianaderer.com>
2024-12-05 14:15:41 -0500

    fixed gallery on mobile

--- components/Gallery/parts/Revolving.js
+++ components/Gallery/parts/Revolving.js
@@ -27,7 +27,7 @@ const Revolving = props => {
             {tagline && <Text tag={'h1'} className={`text-center text-3xl mb-8`}>{tagline}</Text>}
             {galleryParagraph && <Text tag={'p'} className={`text-center text-xl`}>{galleryParagraph}</Text>}
             <div className="relative my-8 ">
-                <div ref={carouselRef} style={{ width: dynamicWidth !== null ? dynamicWidth : 'auto' }} className={`imageGallery flex flex-row gap-4 ${galleryActive ? `xl:max-w-[125%] xl:translate-x-[-50%] xl:ml-[50%]` : `w-fit`} mx-auto drop-shadow-lg border-2 border-secondary/20 rounded-xl`}>
+                <div ref={carouselRef} style={{ width: dynamicWidth !== null ? dynamicWidth : 'auto' }} className={`imageGallery flex flex-row gap-4 max-w-full ${galleryActive ? `xl:max-w-[125%] xl:translate-x-[-50%] xl:ml-[50%]` : `w-fit max-w-[100%]`} mx-auto drop-shadow-lg border-2 border-secondary/20 rounded-xl`}>
                     <Carousel active={galleryActive}>
                         {Object.keys(imageGallery).map((index, idx) => {
                             const image = imageGallery[index];
